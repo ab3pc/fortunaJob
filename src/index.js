f070f50e65eaf6963821b6940b5c406c819dc71b
AleksandrBorysenko <3pc@ukr.net>
2021-10-18 16:27:08 +0300

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "../../react-fortunajob/src/css/style.css";
+import "../src/css/style.css"
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
