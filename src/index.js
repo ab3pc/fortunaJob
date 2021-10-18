bf624b2f76f3b6109654aed14e91ed7151f92f87
AleksandrBorysenko <3pc@ukr.net>
2021-10-18 15:26:39 +0300

    yarn.lock

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import "../../react-fortunajob/src/css/style.css";
 import { BrowserRouter } from "react-router-dom";
